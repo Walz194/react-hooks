adfd6272d3b89166c0829660ff32eb6f6df01af8
Olawale <olawale.akinodanye@gmail.com>
2021-01-11 09:30:20 +0100

    Started using useReducer hook

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import Todo from "./components/Todo";
+import Header from "./components/Header";
+import Auth from "./components/Auth";
+import AuthContext from "./auth-context";
+
+function App(props) {
+  const [page, setPage] = useState("auth");
+  const [authStatus, setAuthStatus] = useState(false);
+  const switchPage = (pagename) => {
+    setPage(pagename);
+  };
+  const login = () => {
+    setAuthStatus(true);
+  };
+  return (
+    <div className="App">
+      <AuthContext.Provider value={{ status: authStatus, login: login }}>
+        <Header
+          onLoadTodos={() => switchPage("todos")}
+          onLoadAuth={() => switchPage("auth")}
+        />
+        <hr />
+        {page === "auth" ? <Auth /> : <Todo />}
+      </AuthContext.Provider>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Todo.js
@@ -0,0 +1,106 @@
+import React, { useState, useEffect, useReducer } from "react";
+import axios from "axios";
+
+const Todo = (props) => {
+  const [todoName, setTodoName] = useState("");
+  //const [todoList, setTodoList] = useState([]);
+  const [submittedTodo, setSubmittedTodo] = useState(null);
+
+  const todoListReducer = (state, action) => {
+    switch (action.type) {
+      case "ADD":
+        return state.concat(action.payload);
+      case "SET":
+        return action.payload;
+      case "REMOVE":
+        return state.filter((todo) => todo.id !== action.payload);
+      default:
+        return state;
+    }
+  };
+
+  const [todoList, dispatch] = useReducer(todoListReducer, []);
+
+  useEffect(() => {
+    axios
+      .get("https://todo-app-270d6-default-rtdb.firebaseio.com/todo.json")
+      .then((res) => {
+        console.log(res.data);
+        const todoData = res.data;
+        let todos = [];
+        for (const key in todoData) {
+          todos.push({ id: key, name: todoData[key].name });
+        }
+        dispatch({ type: "SET", payload: todos });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  useEffect(() => {
+    if (submittedTodo) {
+      dispatch({ type: "ADD", payload: submittedTodo });
+    }
+  }, [submittedTodo]);
+
+  const inputChangeHandler = (event) => {
+    setTodoName(event.target.value);
+  };
+
+  const todoAddHandler = () => {
+    axios
+      .post("https://todo-app-270d6-default-rtdb.firebaseio.com/todo.json", {
+        name: todoName,
+      })
+      .then((res) => {
+        console.log(res);
+        const todoItem = { id: res.data.name, name: todoName };
+        setSubmittedTodo(todoItem);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const todoRemoveHandler = (todoId) => {
+    axios
+      .delete(
+        `https://todo-app-270d6-default-rtdb.firebaseio.com/todo/${todoId}.json`
+      )
+      .then(dispatch({ type: "REMOVE", payload: todoId }))
+      .catch((err) => console.log(err));
+  };
+
+  return (
+    <React.Fragment>
+      <form>
+        <div className="form-group">
+          <input
+            onChange={inputChangeHandler}
+            className="form-control"
+            value={todoName}
+            placeholder="Todo"
+            type="text"
+          />
+          <button
+            className="btn btn-primary"
+            onClick={todoAddHandler}
+            type="button"
+          >
+            ADD
+          </button>
+          <ul className="list-unstyled justify-self-center">
+            {todoList.map((todo) => (
+              <li onClick={() => todoRemoveHandler(todo.id)} key={todo.id}>
+                {todo.name}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </form>
+    </React.Fragment>
+  );
+};
+
+export default Todo;
